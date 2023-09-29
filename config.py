450269e75696dc0b4d4c113fe353775039665a8f
Mas73rs <44162345+Mas73rs@users.noreply.github.com>
2023-09-29 01:47:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,15 @@ cleaned_data_path = "../data/processed/cleaned_data.csv"
 
 # Path to the model-ready data file
 models_data_path = "../data/processed/models_data.csv"
+
+# Path to the X_train data file
+X_train_path = "../data/processed/X_train.csv"
+
+# Path to the X_test data file
+X_test_path = "../data/processed/X_test.csv"
+
+# Path to the y_train data file
+y_train_path = "../data/processed/y_train.csv"
+
+# Path to the y_test data file
+y_test_path = "../data/processed/y_test.csv"
