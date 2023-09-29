801a49fd5211b7b0ab056e081d3fcb8a3a01c9d2
Mas73rs <44162345+Mas73rs@users.noreply.github.com>
2023-09-29 01:43:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ raw_data_path = "../data/raw/HR_dataset.csv"
 
 # Path to the cleaned data file
 cleaned_data_path = "../data/processed/cleaned_data.csv"
+
+# Path to the model-ready data file
+cleaned_data_path = "../data/processed/models_data.csv"
